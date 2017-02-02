cf4583678f44c044095d1fcfdefa55aee57a7598
Piotr Gularski <piotr.gularski@gmail.com>
2017-02-02 17:34:57 +0000

    Nothing there yet, just empty files

---
+++ lexer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Token(object):
+    def __init__(self):
+        pass
+
+
+class TokenList(object):
+    def __init__(self):
+        self.tokens = []
+
+
+class Lexer(object):
+    def __init__(self):
+        pass
+
+    def tokenize(self, input_string):
+        pass
---
+++ parser.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Parser(object):
+    def __init__(self):
+        pass
+
+    def parse(self, tokens):
+        pass
